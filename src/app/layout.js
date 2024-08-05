0e44fbb0f4a47d2e6173a68524a36d63ee9709cd
Syedumair1011 <145678458+Syedumair1011@users.noreply.github.com>
2024-08-05 12:00:42 +0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ImportBs />
         <div>{children}</div>
-        <GoogleTagManager gtmId="G-MERPMJTGVD" />
+        <GoogleTagManager gtmId="GTM-K3G7FFWJ" />
       </body>
     </html>
   )
